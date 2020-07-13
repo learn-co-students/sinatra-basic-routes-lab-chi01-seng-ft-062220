0d873e6d57162d80d104a8247130901cdad5be72
Eve Reichmann <eve.reichmann@gmail.com>
2020-07-13 12:37:13 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+         "My name is Eve"
+    end
+
+    get "/hometown" do
+        "My hometown is Bloomingdale"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Body Sinhead Harnett"
+    end
 end
